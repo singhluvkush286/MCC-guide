1b79e03906c7f68e9676b1a1a13e46afcaac7821
Recur-luvkush-singh <luvkush@recur.club>
2023-08-07 20:33:40 +0530

    first commit

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,32 +1,50 @@
-import React from 'react';
+// import React from 'react';
+// import twitterIcon from './images/insta.png';
+// import facebookIcon from './images/twitter.png';
 
-function Footer() {
-  return (
-    <footer className="App-footer" style={footerStyle}>
-      <p>&copy; {new Date().getFullYear()} YourCompany. All rights reserved.</p>
-      <div className="social-media-links">
-        <a href="https://www.example.com/twitter" target="_blank" rel="noopener noreferrer">
-          <img src="/path/to/twitter-icon.png" alt="Twitter" />
-        </a>
-        <a href="https://www.example.com/facebook" target="_blank" rel="noopener noreferrer">
-          <img src="/path/to/facebook-icon.png" alt="Facebook" />
-        </a>
-      </div>
-    </footer>
-  );
-}
+// function Footer() {
+//   return (
+//     <footer className="App-footer" style={footerStyle}>
+//       <p style={copyrightStyle}>&copy; {new Date().getFullYear()} YourCompany. All rights reserved.</p>
+//       <div className="social-media-links" style={socialMediaLinksStyle}>
+//         <a href="https://www.example.com/twitter" target="_blank" rel="noopener noreferrer">
+//           <img src={twitterIcon} alt="Twitter" style={iconStyle} />
+//         </a>
+//         <a href="https://www.example.com/facebook" target="_blank" rel="noopener noreferrer">
+//           <img src={facebookIcon} alt="Facebook" style={iconStyle} />
+//         </a>
+//       </div>
+//     </footer>
+//   );
+// }
 
-const footerStyle = {
-  position: 'fixed',
-  bottom: 0,
-  left: 0,
-  right: 0,
-  height: '30px', // Set the height of the footer here
-  background: '#f0f0f0', // Add any background color you prefer
-  padding: '1rem',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-};
+// const footerStyle = {
+//   width: '100%',
+//   height: '100px', // Adjust the height of the footer here
+//   background: '#333', // Update the background color to a darker shade
+//   color: '#fff', // Set text color to white
+//   padding: '1rem',
+//   display: 'flex',
+//   justifyContent: 'space-between',
+//   alignItems: 'center',
+//   position: 'relative',
+//   marginTop: 'auto',
+// };
 
-export default Footer;
+// const copyrightStyle = {
+//   margin: 0,
+//   fontSize: '14px', // Smaller font size
+// };
+
+// const socialMediaLinksStyle = {
+//   display: 'flex',
+//   alignItems: 'center',
+// };
+
+// const iconStyle = {
+//   width: '24px',
+//   height: '24px',
+//   marginRight: '10px', // Add some space between icons
+// };
+
+// export default Footer;
--- src/Component/Main.js
+++ src/Component/Main.js
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 import MapboxDirectionsComponent from './MapboxDirectionsComponent';
 
-
 const Main = () => {
-    const initialPlaces = [
-        { name: "International Guest House", coordinates: [80.1234, 12.3456] },
-        { name: "Commerce Block", coordinates: [80.1196482, 12.9225965] },
-        { name: "Martin Hall", coordinates: [80.118857, 12.9200416] },
-        { name: "Margret Hall", coordinates: [80.1194167, 12.9203111] },
-        { name: "Barnes Hall", coordinates: [80.119031, 12.9202077] },
-        { name: "Student Centre", coordinates: [80.1227692, 12.9201743] },
-        { name: "IBC", coordinates: [80.1189707, 12.918309] },
-        { name: "Pavilion", coordinates: [80.1168207, 12.9192925] },
-        { name: "Heber Chapel", coordinates: [80.1230878, 12.9197962] }
-    
-      ];
+  const initialPlaces = [
+    { name: "International Guest House", coordinates: [80.1234, 12.3456] },
+    { name: "Commerce Block", coordinates: [80.1196482, 12.9225965] },
+    { name: "Martin Hall", coordinates: [80.118857, 12.9200416] },
+    { name: "Margret Hall", coordinates: [80.1194167, 12.9203111] },
+    { name: "Barnes Hall", coordinates: [80.119031, 12.9202077] },
+    { name: "Student Centre", coordinates: [80.1227692, 12.9201743] },
+    { name: "IBC", coordinates: [80.1189707, 12.918309] },
+    { name: "Pavilion", coordinates: [80.1168207, 12.9192925] },
+    { name: "Heber Chapel", coordinates: [80.1230878, 12.9197962] }
+
+  ];
+
   const [origin, setOrigin] = useState('');
   const [destination, setDestination] = useState('');
   const [showMap, setShowMap] = useState(false);
@@ -35,16 +35,15 @@ const Main = () => {
 
   const handleSearch = () => {
     setShowSearchBox(false);
-  
+
     // Find the selected origin and destination objects based on their names
     const selectedOrigin = initialPlaces.find(place => place.name === origin);
     const selectedDestination = initialPlaces.find(place => place.name === destination);
-  
+
     // Pass the coordinates of the selected origin and destination to the MapboxDirections component
     setOrigin(selectedOrigin.coordinates);
     setDestination(selectedDestination.coordinates);
   };
-  
 
   return (
     <main>
@@ -81,6 +80,9 @@ const Main = () => {
           <MapboxDirectionsComponent
             origin={origin}
             destination={destination}
+            // Add necessary props to control map size on mobile
+            mapWidth="100%" // Set the map width to occupy the entire container on mobile
+            mapHeight="300px" // Set a fixed height or adjust as needed for the best mobile view
           />
         </div>
       )}
